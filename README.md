e410c08f6a8175e630cd129758e769d2da9b37a9
Joel-Turner <joel@theturners.com.au>
2022-12-14 16:50:47 +1100

    Added Autoclicker and Edited README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LEGO Clicker
-This project is cookie clicker but with LEGO
+This project is cookie clicker but with LEGO. It is a work of art and is probably the best web game ever made. Ensure it is properly appreciated.
--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let timesClicked = parseInt(window.localStorage.getItem("timesClicked")) || 0;
 let addInteger = parseInt(window.localStorage.getItem("addInteger")) || 1;
-
-//let timesClicked = 0;
+let ifAutoclick = parseInt(window.localStorage.getItem("ifAutoclick")) || 1;
+let IntervalID = undefined
 
 const clickedTimesDiv = document.getElementsByClassName("clickedTimes")[0]
 
 clickedTimesDiv.innerHTML = timesClicked
-//let addInteger = 1
 
 const count = () => {
     window.localStorage.setItem('timesClicked', timesClicked)
     clickedTimesDiv.innerHTML = timesClicked
 }
-const countincrease = () => {
+
+const countIncrease = () => {
     window.localStorage.setItem('addInteger', addInteger)
+}
+
+const countAutoclick = () => {
+    window.localStorage.setItem('ifAutoclick', ifAutoclick)
 }
 
 const countLEGO = () =>  {  
@@ -21,18 +25,44 @@ const countLEGO = () =>  {
     count()
 }
 
+const autoclickAction = () => {
+    timesClicked += 1
+    count()
+}
+
+if (ifAutoclick === 2){
+    IntervalID = setInterval(autoclickAction, 1000)
+    countAutoclick()
+}
+
+const Autoclick = () => {
+    if (timesClicked >= 1000){
+        if (ifAutoclick === 1){
+            ifAutoclick += 1
+            timesClicked -= 1000
+            count()
+            IntervalID = setInterval(autoclickAction, 1000)
+            countAutoclick()
+        }
+    }   
+}
+
 const increaseAddInteger = () => {
     if (timesClicked >= 100){
         addInteger += 1
         timesClicked -= 100
     }
-    countincrease()
+    countIncrease()
     count()
+    countAutoclick
 }
+
 const Reset = () => {
     addInteger = 1
     timesClicked = 0
-    countincrease()
+    ifAutoclick = 1
+    clearInterval(IntervalID)
+    countIncrease()
     count()
+    countAutoclick()
 }
- 
